1813d9b10395551604081f2cadbc89bf9bdcab4e
Jacob <zhyb292@aucklanduni.ac.nz>
2026-08-27 16:10:33 +1200

    Update change of new c file

---
+++ Lab4_ADC/Pre-lab4/Pre-lab4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Pre-lab4.c
+ *
+ * Created: 27/08/2026 4:06:09 PM
+ * Author : zhyb292
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
