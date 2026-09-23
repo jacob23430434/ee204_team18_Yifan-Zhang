5aedf4900231598a27edefc84b566566b3d9eb90
Yifan Zhang <zhyb292@aucklanduni.ac.nz>
2026-09-24 10:31:54 +1200

    Finish Prelab questions

---
+++ Lab6_Display/Lab6_display/Lab6_display/Debug/src/main.d
@@ -0,0 +1,89 @@
+src/main.d src/main.o: ../src/main.c ../src/asf.h \
+ ../src/ASF/common/services/gpio/gpio.h ../src/ASF/common/utils/parts.h \
+ ../src/ASF/common/services/gpio/mega_gpio/mega_gpio.h \
+ ../src/ASF/mega/utils/compiler.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Application\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328p.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ ../src/ASF/common/utils/interrupt.h \
+ ../src/ASF/common/utils/interrupt/interrupt_avr8.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../src/ASF/mega/utils/progmem.h \
+ d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ ../src/ASF/common/services/ioport/ioport.h \
+ ../src/ASF/common/services/ioport/mega/ioport.h \
+ ../src/ASF/common/boards/board.h \
+ ../src/ASF/mega/boards/atmega328p_xplained_mini/atmega328p_xplained_mini.h \
+ ../src/ASF/mega/boards/atmega328p_xplained_mini/led.h \
+ ../src/ASF/mega/utils/status_codes.h
+
+../src/asf.h:
+
+../src/ASF/common/services/gpio/gpio.h:
+
+../src/ASF/common/utils/parts.h:
+
+../src/ASF/common/services/gpio/mega_gpio/mega_gpio.h:
+
+../src/ASF/mega/utils/compiler.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Application\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328p.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+../src/ASF/common/utils/interrupt.h:
+
+../src/ASF/common/utils/interrupt/interrupt_avr8.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../src/ASF/mega/utils/progmem.h:
+
+d:\application\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+../src/ASF/common/services/ioport/ioport.h:
+
+../src/ASF/common/services/ioport/mega/ioport.h:
+
+../src/ASF/common/boards/board.h:
+
+../src/ASF/mega/boards/atmega328p_xplained_mini/atmega328p_xplained_mini.h:
+
+../src/ASF/mega/boards/atmega328p_xplained_mini/led.h:
+
+../src/ASF/mega/utils/status_codes.h:
